83961d8a9aae942707ffc041ffbc1a1d509f9474
Steven Firth <s.k.firth@lboro.ac.uk>
2021-01-16 07:16:06 +0000

    Updated parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,56 +2,31 @@
 
 import unittest
 
-from gbxml import parser, create_gbXML
+from gbxml import get_parser, create_gbXML
 from lxml import etree
-import gbxml
 
 
 class Test_parser(unittest.TestCase):
    
-    
-    def test_parser(self):
+    def test_create_gbXML(self):
         ""
+        gbXML=create_gbXML()
+        print(gbXML)
+        print(gbXML.attributes)
         
-        
-        
+    
+    
+    def test_get_parser(self):
+        ""
         fp=r'files\gbXMLStandard.xml'
+        
+        parser=get_parser(version='0.37')
+        print(parser)
+        
         tree = etree.parse(fp,parser)
         gbXML=tree.getroot()
-        
         print(gbXML)
-        gbXML.test()
         print(gbXML.Campus)
-        
-        print(gbXML.Campus.Location.CADModelAzimuth)
-        
-        
-        
-        
-    # def test_new_gbXML(self):
-        
-    #     st=gbxml.new_gbXML_string
-    #     #print(st)
-    #     tree = etree.parse(st,parser)
-    #     gbXML=tree.getroot()
-    #     #print(gbXML)
-    #     gbXML.add_Campus()
-    #     #print(etree.tostring(gbXML))
-    #     #print(gbXML.Campus)
-        
-    #     st='<gbXML version="6.01" xmlns="http://www.gbxml.org/schema"></gbXML>'
-    #     gbXML=etree.fromstring(st,parser)
-    #     #print(gbXML)
-        
-        
-    # def test_create_gbXML(self):
-    #     ""
-    #     gbXML=create_gbXML()
-    #     #print(gbXML)
-    #     #print(gbXML.attributes)
-        
-        
-        
         
     
 if __name__=='__main__':
